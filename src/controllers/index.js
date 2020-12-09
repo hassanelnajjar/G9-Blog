f3dc46417ba5ee6c331260d51c2258e2233477ac
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-12-09 14:13:26 +0200

    added get requests

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
 const {
-  addComment, getUserId, addUserName,
+  addComment, getUserId, addUserName, addPost, getPosts, getComments,
 } = require('../database/queries');
+
+// get all posts
+router.get('/posts', (req, res, next) => {
+  getPosts().then((results) => res.json({
+    data: results.rows,
+    status: 200,
+    msg: 'success',
+  })).catch(next);
+});
+
+// get Comments for specific post
+router.get('/comments/:postId', (req, res, next) => {
+  getComments(req.params.postId)
+    .then((results) => res.json({
+      data: results.rows,
+      status: 200,
+      msg: 'success',
+    })).catch(next);
+});
+
 // add Comments
 router.post('/add-comment/:postId/:username', (req, res, next) => {
   getUserId(req.params.username)
@@ -25,11 +45,30 @@ router.post('/add-comment/:postId/:username', (req, res, next) => {
     .catch(next);
 });
 
-router.post('/posts/:id/comments', (req, res, next) => {
-  console.log('ggggg');
-  getPostsbyid(req.params.id)
-    .then()
-    .catch();
+// add post
+router.post('/add-post', (req, res, next) => {
+  getUserId(req.body.username)
+    .then((usernameId) => {
+      if (usernameId.rows.length !== 0) {
+        // eslint-disable-next-line max-len
+        addPost(req.body.text_content, usernameId.rows[0].id)
+          .then(({ rows }) => res.status(200).json({
+            data: rows,
+            msg: 'success',
+            status: 200,
+          })).catch(next);
+        return;
+      }
+      // eslint-disable-next-line max-len
+      addUserName(req.params.username).then((usernameId) => {
+        addPost(req.body.text_content, usernameId.rows[0].id)
+          .then(() => res.status(200).json({
+            data: null,
+            msg: 'success',
+            status: 200,
+          })).catch(next);
+      }).catch(next);
+    }).catch(next);
 });
 
 router.use((err, req, res, next) => {
--- src/database/queries/addPost.js
+++ src/database/queries/addPost.js
@@ -1,9 +1,9 @@
 const connection = require('../config/connection');
 
-const addPost = () => {
+const addPost = (postText, userId) => {
   const sql = {
-    text: 'INSERT INTO posts () values($1 ,$2, $3) returning *',
-    values: [],
+    text: 'INSERT INTO posts (post_text,users_id) values($1 ,$2) returning *',
+    values: [postText, userId],
   };
   return connection.query(sql);
 };

4f4a1d59ab182fb906c3cb853b2fb543266da6f2
Alaa <alaa.sahloub.eb@gmail.com>
2020-12-09 15:37:54 +0200

    resolve conflect and testing for database functions

--- tests/database.test.js
+++ tests/database.test.js
@@ -7,40 +7,40 @@ const addComment = require('../src/database/queries/addComment');
 const addPost = require('../src/database/queries/addPost');
 const getComments = require('../src/database/queries/getComments');
 const getCommentsCounts = require('../src/database/queries/getCommentsCounts');
-const getPosts = require('../src/database/queries/getPosts');
+const { getPosts } = require('../src/database/queries/getPosts');
 
 beforeEach(build);
 
 afterAll(() => connection.end());
 
-test('addComment >> This test will add comment', () => addComment(1, 'alaa', 'Hiiiiiii')
+test('addComment >> This test will add comment', () => addComment('Hiiiiiii', 1, 1)
   .then(() => expect(1).toBe(1))
   .catch());
 
-test('addPost >> This test will add post', () => addPost('hiiiiii', 'alaa')
+test('addPost >> This test will add post', () => addPost('hiiiiii', 1)
   .then(() => expect(1).toBe(1))
   .catch());
 
 test('getComments >> This test will return comments data', () => getComments(1)
   .then((result) => {
-    const reslen = result.length;
-    const expected = 5;
+    const reslen = result.rows.length;
+    const expected = 1;
     expect(reslen).toBe(expected);
   })
   .catch());
 
 test('getCommentsCounts >> This test will return comments count', () => getCommentsCounts(1)
   .then((result) => {
-    const reslen = result;
-    const expected = 5;
+    const reslen = result.rowCount;
+    const expected = 1;
     expect(reslen).toBe(expected);
   })
   .catch());
 
 test('getPosts >> This test will return posts data', () => getPosts()
   .then((result) => {
-    const reslen = result.length;
-    const expected = 5;
+    const reslen = result.rows.length;
+    const expected = 2;
     expect(reslen).toBe(expected);
   })
   .catch());
